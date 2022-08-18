c5de2d9d9e58ec15b90cd90d0ec41597ee107da6
Ren <tianyin.rien@gmail.com>
2022-08-18 08:46:31 +0800

    nothing

--- switch.sh
+++ switch.sh
@@ -3,7 +3,7 @@
 windows=$(wmctrl -xl | tr -s '[:blank:]' | cut -d ' ' -f 5- | sort | uniq)
 st_w=$(echo "$windows" | grep -vE ".*pdf")
 windows_m=$(echo -e "${st_w}")
-target=$(echo "$windows_m" | dmenu -l 10 -i -p 'Switch ' -x 220 -y 1000 -z 2120)
+target=$(echo "$windows_m" | dmenu -l 10 -i -x 220 -y 1000 -z 2120)
 
 if [[ -n $target ]]; then
         wmctrl -a "$target"
