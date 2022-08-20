bb301a73adf2abe5da4243ac054749e19cd529f4
Ren <tianyin.rien@gmail.com>
2022-08-20 13:10:18 +0800

    update

--- dmenu-wifi.sh
+++ dmenu-wifi.sh
@@ -8,29 +8,34 @@ then
 fi
 
 sudo pkill wpa_supplicant && sudo iw dev wlan0 disconnect
-sudo pkill v2ray 
+#sudo pkill v2ray 
 
 case "$sel" in
     oneplus)
-           sudo pkill wpa_supplicant 
-           sudo wpa_supplicant -i wlan0 -c ~/scripts/oneplus.conf -B 
-		   v2ray -c ~/scripts/config.json & ;;
+       sudo pkill wpa_supplicant 
+       sudo wpa_supplicant -i wlan0 -c ~/scripts/oneplus.conf -B 
+#	   v2ray -c ~/scripts/config.json & 
+	   ;;
        home)
 	   sudo pkill wpa_supplicant
 	   sudo wpa_supplicant -i wlan0 -c ~/scripts/home.conf -B 
-	   v2ray -c ~/scripts/config.json & ;;
+#	   v2ray -c ~/scripts/config.json & 
+	   ;;
        cnu)
 	   sudo iw dev wlan0 disconnect
 	   sudo iw dev wlan0 connect CNU
-	   v2ray -c ~/scripts/config.json & 
+#	   v2ray -c ~/scripts/config.json & 
 	   sleep 5
-	   qutebrowser http://192.168.1.91 ;;
+	   qutebrowser http://192.168.1.91 
+	   ;;
     wpa-off)
 	   sudo pkill wpa_supplicant
-	   sudo pkill v2ray ;;
+#	   sudo pkill v2ray
+	   ;;
     iw-off)
 	   sudo iw dev wlan0 disconnect
-	   sudo pkill v2ray ;;
+#	   sudo pkill v2ray
+	   ;;
 esac
 
 exit 0
--- wakeup.sh
+++ wakeup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for device in XHC1 LID0; do
+for device in XHC1 RP01 RP02 RP03 RP05 RP06; do
     grep $device /proc/acpi/wakeup | grep enabled > /dev/null && {
         echo Disabling wakeup on $device
         echo $device > /proc/acpi/wakeup
